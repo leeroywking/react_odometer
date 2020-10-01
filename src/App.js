09438727a163502ac9f52fdba5eca9298370bc4b
Lee-Roy King <leeroywking@gmail.com>
2020-09-30 20:22:13 -0700

    basic features complete though I'm kind of sad about it

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Odometer from "./components/odometer/odometer";
+import sleep from "./components/sleep";
+
 
 function App() {
+  const [odo, setOdo] = useState(0);
+  function addX(x) {
+    x = parseInt(x);
+    setOdo(odo + x);
+  }
+
+  function AddButton(props) {
+    return <button onClick={() => addX(props.num)}>Add {props.num}</button>;
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello World!</p>
+        <Odometer odometer={odo} />
+        <span>
+          <AddButton num="10000" />
+          <AddButton num="1000" />
+          <AddButton num="100" />
+          <AddButton num="10" />
+          <AddButton num="1" />
+        </span>
       </header>
     </div>
   );
